fc26db02dafe9a26bda2ddb5e1f09bdb639ade60
karalkal <karalkal@yahoo.co.uk>
2023-05-19 07:19:28 +0100

    added evenlisteners for clicked buttons

--- src/events.js
+++ src/events.js
@@ -1,4 +1,33 @@
-function func1() {
-    $("#btn1").css("background-color", "red")
+$("document").ready(() => {
+    // as soon as page loads change styling of selected menu item
+    $("#btn1").css("background-color", "var(--cerise)");
+    $("#btn1>a").css("color", "var(--eerie-black)");
+
+    // change style and img src
+    $("#change-css-btn").click(() => {
+        $("#canvas--events").css({
+            "background-color": "var(--celestial-blue",
+            border: "8px dotted var(--cerise)"
+        });
+        $("li:even, h2").css("font-style", "italic");
+        $("li:odd").css("font-weight", "bold");
+        $("p").css("background-color", "white")
+        $("img").attr("src", "img/pexels-beyzaa-yurtkuran-16130228.jpg")
+    })
+
+    // fadeToggle
+    $("#toggle-fade-btn").click(() => {
+        $("p").fadeToggle()
+
+    })
+
+
+
 }
+)
+
+
+
+
+
 
