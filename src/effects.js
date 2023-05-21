5322f190d3c756393c82a15f3f8bcb03c625681b
karalkal <karalkal@yahoo.co.uk>
2023-05-21 02:27:19 +0100

    created skeleton for effects demo, added effects table

---
+++ src/effects.js
@@ -0,0 +1,13 @@
+$("document").ready(() => {
+    // as soon as page loads change styling of current page menu item
+    $("#btn2").css("background-color", "var(--gunmetal)");
+    $("#btn2>a").css("color", "white");
+
+
+})
+
+
+
+
+
+
